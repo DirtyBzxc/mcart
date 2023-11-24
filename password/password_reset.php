878d5dedfd113387338a69cd96080bc3d929f110
Yersaiyn <ezhahin@gmail.com>
2023-11-24 17:22:36 +0300

    test_commit

--- password/password_reset.php
+++ password/password_reset.php
@@ -1,7 +1,7 @@
 <?
 require($_SERVER['DOCUMENT_ROOT']."/bitrix/header.php");
 $USER->Update(1,array("LOGIN"=>'test123'));
-$USER->Update(1,array("PASSWORD"=>'Bitrix*123456'));
+$USER->Update(1,array("PASSWORD"=>'Bitrix*1234567'));
 echo 'Вы успешно изменили данные для входа в панель администратора.';
 require($_SERVER['DOCUMENT_ROOT']."/bitrix/footer.php");
 ?>
